f73c80926787a2f19157d11633f6384aa4c5d39c
SethGrah <sethegraham8@gmail.com>
2022-08-08 21:30:10 -0400

    final fr this

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://team11rentalbackend-env.eba-c343hbjn.us-east-1.elasticbeanstalk.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://team11rentalbackend-env.eba-c343hbjn.us-east-1.elasticbeanstalk.com/'
 };
 
 /*
@@ -14,4 +14,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+//  team11-car-db.c4eqo06kg56i.us-east-1.rds.amazonaws.com
